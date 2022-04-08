9dcc4f48b08b2d6682f1406be52738f943b4323d
islomfrontdev <islomfrontdev@gmail.com>
2022-04-08 18:52:13 +0500

    build

--- src/pages/Admin/statement.js
+++ src/pages/Admin/statement.js
@@ -1,16 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const [statements, setStatement] = useState([
-  {name : "Islom Shahobiddinov",
-  food : "Pitsa",
-    price: "50000",
-    num: "2",
-    amount: "100000",
-  
-  }
-])
 export default function Statement() {
-  return (
-    <div>Statement</div>
-  )
+  const [statements, setStatement] = useState([
+    {
+      name: "Islom Shahobiddinov",
+      food: "Pitsa",
+      price: "50000",
+      num: "2",
+      amount: "100000",
+    },
+  ]);
+  return <div>Statement</div>;
 }
